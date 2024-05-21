381a50e2a9ed7ad6b0ca51fba49a3f9dce0acb8a
Lev Lazinskiy <lev@dagger.io>
2024-05-21 09:35:48 -0700

    fix plain text stuff

--- lua/plaintext.lua
+++ lua/plaintext.lua
@@ -1,6 +1,20 @@
 -- Plain Text editing
 vim.api.nvim_create_autocmd({ "FileType"}, {
-    pattern = 'mail,gitcommit,text,markdown',
+    pattern = 'mail,text,markdown',
+    callback = function()
+        vim.opt.wrap = true
+        vim.opt.linebreak = true
+        vim.opt.list = false
+        vim.opt.spell = true
+        vim.opt.spelllang="en_us"
+        require("cmp").setup.buffer({enabled = false})
+    end
+})
+
+-- Git Commit editing
+-- TODO: can we make this an if instead?
+vim.api.nvim_create_autocmd({ "FileType"}, {
+    pattern = 'gitcommit',
     callback = function()
         vim.opt.textwidth = 72
         vim.opt.spell = true 
